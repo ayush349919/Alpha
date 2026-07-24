d12e8b4e6e8ea804a49db901487314b208d051c2
ayush <ayush349919@gmail.com>
2026-07-25 00:59:02 +0530

    did Some Experiments

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const dns = require("dns");
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const dotenv = require("dotenv");
+
 dotenv.config();
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
@@ -13,12 +14,17 @@ const authRoutes = require("./routes/auth.route");
 const userRoutes = require("./routes/rolebased/user/user.route");
 
 connectToMongoDB();
-
 const app = express();
 // app.use(helmet)
 app.use(morgan("dev"));
 
-app.use(cors());
+const corsOptions = {
+  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  credentials: true,
+  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
